14d3475a1b9b1b02b48c7276e879a4e5511177a0
Бикчурин Эльмир Шамильевич <e.bikchurin@faberlic.com>
2024-10-10 20:29:15 +0300

    v0 fix public_html

--- public_html/index.php
+++ public_html/index.php
@@ -15,11 +15,3 @@ require __DIR__ . '/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__ . '/../bootstrap/app.php')
     ->handleRequest(Request::capture());
-$app = new Illuminate\Foundation\Application(
-    realpath(__DIR__ . '/../')
-);
-
-$app->bind('path.public', function ()
-{
-    return base_path() . '/public_html';
-});
