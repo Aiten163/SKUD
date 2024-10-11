3fa85dfd8d7b83736c3c3761a22c85660b569606
lary163 <uuum1984@mail.ru>
2024-10-11 11:12:36 +0300

    v0 fix timestamps

--- app/Models/Door.php
+++ app/Models/Door.php
@@ -8,10 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Door extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
     protected $fillable =
         [
             'id',
             'level',
+            'build',
             'owner',
             'number'
         ];
