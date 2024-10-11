3e733a1f7ff5fb2082c82dc0280e38bd9da69834
lary163 <uuum1984@mail.ru>
2024-10-11 11:48:39 +0300

    v0 test api

--- app/Http/Controllers/API/DoorController.php
+++ app/Http/Controllers/API/DoorController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Card;
+use App\Models\Door;
 use Illuminate\Http\Request;
 
 class DoorController extends Controller
 {
     public function door($id, $room_id)
     {
-        if ($id == 1) {
+        $Card = Card::where('id', $id)->get();
+        $Door = Door::where('room_id', $room_id)->get();
+
+        if ($id) {
             return response()->json([
-                'status' => true
+                'status' => $Card,
+                'door' => $Door
             ], 200);
         } else {
             return response()->json([
